c1bd600d8bc8db2c80446b9ee30c8a298abff448
VivekRaj0 <vivekraj152089@gmail.com>
2026-06-22 20:57:53 +0530

    APIs created for employees, auth, attendance, profile

---
+++ server/routes/authRoutes.js
@@ -0,0 +1,11 @@
+import { Router } from "express"; 
+import { changePassword, login, session } from "../controllers/authController.js";
+import { protect } from "../middleware/auth.js";
+
+const authRouter = Router();
+
+authRouter.post("/login", protect, login)
+authRouter.get("/session", protect, session)
+authRouter.post("/change-password", changePassword)
+
+export default authRouter;
